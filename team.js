ed812b596f3a0a5e34891864ea59ac0b34da6349
Shashider Kadamanchi <nwrockets69@gmail.com>
2025-01-16 17:02:14 -0500

    real

---
+++ team.js
@@ -0,0 +1,152 @@
+const alford = document.getElementById("r");
+const keh = document.getElementById("n");
+const willy = document.getElementById("w");
+const cubehead = document.getElementById("b");
+const max = document.getElementById("m");
+const tobias = document.getElementById("t");
+const bruno = document.getElementById("bh");
+const shashider = document.getElementById("s");
+const tristante = document.getElementById("tc");
+const vindinshi = document.getElementById("v");
+const yish = document.getElementById("y");
+const rayden = document.getElementById("ra");
+const lucas = document.getElementById("l");
+const dom = document.getElementById("d");
+const cheyenne = document.getElementById("c");
+const dusty = document.getElementById("cd");
+const sarah = document.getElementById("sr");
+const log = document.getElementById("lo");
+const text = document.getElementById("changetext");
+const text2 = document.getElementById("changetext1");
+const text3 = document.getElementById("changetext2");
+
+
+alford.addEventListener("click", function() {
+  text.textContent = ("Robo Vikings #9990 Coach and Mentor");
+  text2.textContent = ("Teacher/Mentor");
+  text3.textContent = ("Alternate Role: N/A");
+});
+
+keh.addEventListener("click", function() {
+  text.textContent = ("Robo Vikings #9990 Secondary Coach and Mentor");
+  text2.textContent = ("Mentor/Supervisor");
+  text3.textContent = ("Alternate Role: N/A");
+  
+});
+
+willy.addEventListener("click", function() {
+  text.textContent = ("Team Co-Captain");
+  text2.textContent = ("Senior");
+  text3.textContent = ("Alternate Role: Builder");
+  
+});
+
+cubehead.addEventListener("click", function() {
+  text3.textContent = ("Alternate Role: Team Mascot, Builder");
+  text2.textContent = ("SUPER Senior");
+  text.textContent = ("Team Co-Captain");
+});
+
+max.addEventListener("click", function() {
+  text.textContent = ("Lead Programmer");
+  text2.textContent = ("Senior");
+  text3.textContent = ("Alternate Role: Builder");
+  
+});
+
+tobias.addEventListener("click", function() {
+  text.textContent = ("Team Leader");
+  text2.textContent = ("Junior");
+  text3.textContent = ("Alternate Role: Programmer");
+  
+});
+
+bruno.addEventListener("click", function() {
+  text.textContent = ("Lead Builder");
+  text2.textContent = ("Junior");
+  text3.textContent = ("Alternate Role: N/A");
+  
+});
+
+shashider.addEventListener("click", function() {
+  text.textContent = ("Lead Docs & Lead Organization");
+  text2.textContent = ("Junior");
+  text3.textContent = ("Alternate Role: N/A");
+  
+});
+
+tristante.addEventListener("click", function() {
+  text.textContent = ("Builder");
+  text2.textContent = ("Junior");
+  text3.textContent = ("Alternate Role: N/A");
+  
+});
+
+vindinshi.addEventListener("click", function() {
+  text.textContent = ("Lead Outreach & Lead Portfolio");
+  text2.textContent = ("Sophomore");
+  text3.textContent = ("Alternate Role: N/A");
+  
+});
+
+yish.addEventListener("click", function() {
+  text.textContent = ("Yash");
+  text2.textContent = ("Sophomore");
+  text3.textContent = ("Alternate Role: Everything");
+  
+});
+
+rayden.addEventListener("click", function() {
+  text.textContent = ("Builder");
+  text2.textContent = ("Sophomore");
+  text3.textContent = ("Alternate Role: Moral Support and Chill Guy");
+  
+});
+
+lucas.addEventListener("click", function() {
+  text.textContent = ("Secondary Programmer");
+  text2.textContent = ("Sophomore");
+  text3.textContent = ("Alternate Role: Chill Guy");
+  
+});
+
+dom.addEventListener("click", function() {
+  text.textContent = ("Builder & Organization");
+  text2.textContent = ("Sophomore");
+  text3.textContent = ("Alternate Role: N/A");
+  
+});
+
+cheyenne.addEventListener("click", function() {
+  text.textContent = ("Portfolio & Outreach");
+  text2.textContent = ("Sophomore");
+  text3.textContent = ("Alternate Role: N/A");
+  
+});
+
+dusty.addEventListener("click", function() {
+  text.textContent = ("Secondary Builder");
+  text2.textContent = ("Freshman");
+  text3.textContent = ("Alternate Role: N/A");
+  
+});
+
+sarah.addEventListener("click", function() {
+  text.textContent = ("Portfolio & Outreach");
+  text2.textContent = ("Freshman");
+  text3.textContent = ("Alternate Role: N/A");
+  
+});
+
+log.addEventListener("click", function() {
+  text.textContent = ("Programmer");
+  text2.textContent = ("Freshman");
+  text3.textContent = ("Alternate Role: N/A");
+  
+});
+
+
+
+
+
+
